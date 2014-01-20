650911ef008fd356aff2ec39cc595f224e66c6ed
Conor Sheehan <conor@cantinaconsulting.com>
2014-01-20 11:40:11 -0500

    Client side app draft

--- js_app/scripts/main.js
+++ js_app/scripts/main.js
@@ -1,18 +1,23 @@
-require(["vendor/underscore/underscore-min", "vendor/zepto/zepto"], function(util) {
+require(["vendor/underscore/underscore-min", "vendor/jquery-1.9.1", "vendor/jquery-ui", "poll"], function(util) {
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
     //the module value for "helper/util".
-    var modules = [
-        "RequireJS",
-        "Underscore",
-        "Zepto",
-    ];
+    
+    // var modules = [
+    //     "RequireJS",
+    //     "Underscore",
+    //     "Zepto",
+    // ];
 
-    var $moduleList = $("#module-list");
-    $moduleList.empty();
-    _.each(modules, function(module) {
-        $moduleList.append("<li>" + module + "</li>");
-    });
+    // var $moduleList = $("#module-list");
+    // $moduleList.empty();
+    // _.each(modules, function(module) {
+    //     $moduleList.append("<li>" + module + "</li>");
+    // });
+
+    $('ul.features');
+
+    window.pollApp.Poll.init($('div.main'));
 
 });
--- js_app/scripts/poll.js
+++ js_app/scripts/poll.js
@@ -1,14 +1,16 @@
-window.rll = window.rll || {};
-window.rll.Pages = window.rll.Pages || {};
+// window.rll = window.rll || {};
+// window.rll.Pages = window.rll.Pages || {};
+window.pollApp = window.pollApp || {};
 
 (function(
+    pollApp,
     $,
     _) {
     'use strict';
 
     var self;
 
-    var Poll = {
+    pollApp.Poll = {
 
         // Instantiates the poll
         init: function($el) {
@@ -29,19 +31,22 @@ window.rll.Pages = window.rll.Pages || {};
             self.$el = $el;
             self.$featureListEl = self.$el.find('ul.features');
             self.renderFeatureList(self.features);
+            self.makeSortable(self.$featureListEl);
+
+            self.setupListeners();
         },
 
         // Return sorted array of features, based on current arrangement
         getSortedFeatureList: function() {
-            var $featureListElements = $self.$featureListEl.find('li');
+            var $featureListElements = self.$featureListEl.find('li');
             return _.map($featureListElements, self.getFeatureFromLi);
         },
 
         // Return mapping of sorted features, based on current arrangement
         // ajaxData = True: Map the keys to match the expected server-side keys
         getSortedFeatureMap: function(ajaxData) {
-            var sortedFeatureList = self.getSortedFeatureList();
-            var keys = _.range(1, sortedFeatureList.length + 1);
+            var sortedFeatureList = self.getSortedFeatureList(),
+                keys = _.range(1, sortedFeatureList.length + 1);
             if(ajaxData) {
                 keys = _.map(keys, function(key) {
                     return "feature-" + key;
@@ -51,14 +56,14 @@ window.rll.Pages = window.rll.Pages || {};
         },
 
         // Return feature name from its <li>
-        getFeatureFromLi: function($featureLi) {
-            var featureId = $featureLi.data("feature");
+        getFeatureFromLi: function(featureLi) {
+            var featureId = $(featureLi).data("feature");
             return self.featureMap[featureId];
         },
 
         formSubmit: function() {
-            var url = "http://py.czsheez.com/ecomm-poll/create/";
-            var sortedFeatureMap = self.getSortedFeatureMap();
+            var url = "http://py.czsheez.com/ecomm-poll/create/",
+                sortedFeatureMap = self.getSortedFeatureMap();
             console.log(sortedFeatureMap);
 
             $.post(url, sortedFeatureMap)
@@ -78,23 +83,30 @@ window.rll.Pages = window.rll.Pages || {};
         formAlways: function() {
             console.log('Always');
         },
+
+        makeSortable: function($el) {
+            $el.sortable();
+            $el.disableSelection();
+        },
+
         // Renders the feature list to the page
         renderFeatureList: function(features) {
             self.$featureListEl.empty();
             _.each(features, function(featureId) {
-                var renderedFeature = renderFeatureListItem(featureId);
-                self.featureListElement.append(renderedFeature);
+                var renderedFeature = self.renderFeatureListItem(featureId);
+                self.$featureListEl.append(renderedFeature);
             });
         },
 
         // Return rendered <li> for a feature
         renderFeatureListItem: function(featureId) {
-            var feature = self.featureMap[featureId];
-            return '<li class="ui-state-default" data-feature="' + featureId + '"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>' + feature + '</li>';
+            var feature = self.featureMap[featureId],
+                html = '<li class="ui-state-default" data-feature="' + featureId + '"><i class="fa fa-sort"></i> ' + feature + '</li>';
+            return $(html);
         },
 
         setupListeners: function() {
-            this.$el.find('form').submit(function(e) {
+            self.$el.find('form').submit(function(e) {
                 e.preventDefault();
                 self.formSubmit();
             });
@@ -102,5 +114,6 @@ window.rll.Pages = window.rll.Pages || {};
 
     };
 })(
-    jQuery,
+    window.pollApp,
+    window.$,
     window._);
