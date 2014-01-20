6b95be8a724d1e61281fb2b541713b7b433cc8f3
Conor Sheehan <conor@cantinaconsulting.com>
2014-01-20 12:18:59 -0500

    Responsive width

--- js_app/scripts/main.js
+++ js_app/scripts/main.js
@@ -1,4 +1,4 @@
-require(["vendor/underscore/underscore-min", "vendor/jquery-1.9.1", "vendor/jquery-ui", "poll"], function(util) {
+require(["vendor/underscore/underscore-min", "vendor/jquery-1.9.1", "vendor/jquery-ui", "vendor/jquery-ui-touch-punch", "poll"], function(util) {
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
